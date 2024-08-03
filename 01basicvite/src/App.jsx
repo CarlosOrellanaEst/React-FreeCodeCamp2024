dbae4410a91af024b39bb35027a20884fe7b5ebf
Carlos Orellana <carlos.orellana.obando@est.una.ac.cr>
2024-08-03 17:08:58 -0600

    Custom React
    Custom React: Getting a html document and adding content from an object defined in JS, emulating how React works.
    
    Also redefining that JS object as a React.createElement in the Vite project.

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,13 +1,14 @@
 import Youtube  from './Youtube.jsx'
 
 function App() {
-
+  const username = "Carlos"
   return (
+ /*  inside the {} we only should pass values already evaluated, not values that need to be evaluated */  
     <>
-      <h1>Vite react app</h1>
+      <h1>Vite react app {2+2}</h1>
+      <h1>Vite react app {username}</h1> 
       <Youtube/>
     </>
-
   )
 }
 
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// How we define it in our customReact
+const reactElement = {
+  type: 'a',
+  props: {
+      href: "https://google.com",
+      target: '_blank'
+  },
+  children: 'Click me to visit google'
+}
+
+// How React needs us to define it
+const areactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: "__blank"},
+  'click to visit google'
+)
+
+const AnotherElement = (
+  <a href='http://google.com' target='__blank'>Visit Google</a>
+)
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <App/>
 )
+
+
+
