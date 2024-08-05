c516d0257047fb755b9a1cc4ac76abfa49d3d04e
Carlos Orellana <carlos.orellana.obando@est.una.ac.cr>
2024-08-04 19:43:23 -0600

    State in React.js
    Use of state in functional components:
    
    setCounter((prevCounter) => prevCounter + 1)  for single & isolated updates.
    setCounter(counter + 1) for multiple, sequential updates.

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // const [variable, function]
+  const [counter, setCounter] = useState(15)
+
+ // let counter = 15;
+  const addValue = () => {
+    // for one single, isolated update of the state
+  /*   setCounter(counter+1) */ 
+
+  // for multiple secuencial updates of the State
+    setCounter((previous) => previous+1)
+    setCounter((previous) => previous+1)
+    setCounter((previous) => previous+1)
+    setCounter((previous) => previous+1)
+  }
+  const removeValue = () => {
+/* setCounter(counter-1) */
+    setCounter((previous) => previous-1)
+    setCounter((previous) => previous-1)
+    setCounter((previous) => previous-1)
+    setCounter((previous) => previous-1)
+  }
+
+  return (
+    <>
+      <h1>React course with Hitesh, FreeCodeCamp {counter} </h1>
+      <h2>Counter value: {counter} </h2>
+      <button onClick={addValue}>Add value</button>
+      <button onClick={removeValue}>remove value</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
